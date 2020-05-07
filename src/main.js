4415ded3c32963a43673dad8a3ae3572be793c1a
55036662 <zhippuden@gmail.com>
2020-05-07 19:00:26 +0200

    apollo client mounted, now the client can consume the API.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,42 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import ApolloClient from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+
+const httpLink = createHttpLink({
+	uri: 'http://localhost:5000/graphql',
+});
+
+const authLink = setContext(() => {
+	const token = localStorage.getItem('jwtToken');
+	return {
+		headers: {
+			Authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
+const apolloClient = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
 
 Vue.config.productionTip = false;
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+	defaultClient: apolloClient,
+});
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+	router,
+	store,
+	apolloProvider,
+	render: (h) => h(App),
+}).$mount('#app');
